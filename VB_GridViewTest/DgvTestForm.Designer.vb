5445d624fe4fcadd4bd558b2f8eda659d9082fce
miko <miko.kento@gmail.com>
2018-08-20 20:28:43 +0900

    プロジェクト ファイルを追加します。

---
+++ VB_GridViewTest/DgvTestForm.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.dgView = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.dgView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgView
+        '
+        Me.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.dgView.Location = New System.Drawing.Point(12, 12)
+        Me.dgView.Name = "dgView"
+        Me.dgView.RowTemplate.Height = 21
+        Me.dgView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.dgView.Size = New System.Drawing.Size(583, 391)
+        Me.dgView.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        Me.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        Me.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic
+        '
+        'Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(607, 415)
+        Me.Controls.Add(Me.dgView)
+        Me.Name = "Form"
+        Me.Text = "Form"
+        CType(Me.dgView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgView As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+End Class
---
+++ VB_GridViewTest/DgvTestForm.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'データ追加
+        For i As Integer = 1 To 10
+            dgView.Rows.Add(i.ToString(), "テストデータ" + i.ToString())
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' データグリッドビューでのマウスクリック時処理
+    ''' </summary>
+    ''' <param name="sender">イベント送信元</param>
+    ''' <param name="e">イベント引数</param>
+    Private Sub dgView_MouseDown(sender As Object, e As MouseEventArgs) Handles dgView.MouseDown
+
+        ' 左クリックの場合
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+
+            Dim dgv As DataGridView = CType(sender, DataGridView)
+
+            Select Case dgv.HitTest(e.X, e.Y).Type
+                ' 全選択処理の場合
+                Case Is = DataGridViewHitTestType.TopLeftHeader
+                    dgv.ClearSelection()
+                    dgv.SelectAll()
+
+                ' 列選択の場合
+                Case Is = DataGridViewHitTestType.ColumnHeader
+
+                    ' 列選択モードに切り替え
+                    dgv.SelectionMode = DataGridViewSelectionMode.ColumnHeaderSelect
+
+                    ' 選択対象外のセルは選択状態を解除する
+                    If Not dgv.SelectedColumns.Contains(dgv.Columns(dgv.CurrentCell.ColumnIndex)) Then
+                        dgv.CurrentCell.Selected = False
+                    End If
+
+                ' 行選択の場合
+                Case Is = DataGridViewHitTestType.RowHeader
+                    ' 行選択モードに切り替える
+                    dgv.SelectionMode = DataGridViewSelectionMode.RowHeaderSelect
+
+                    ' 選択対象外のセルは選択状態を解除する
+                    If Not dgv.SelectedRows.Contains(dgv.Rows(dgv.CurrentCell.RowIndex)) Then
+                        dgv.CurrentCell.Selected = False
+                    End If
+            End Select
+        End If
+    End Sub
+End Class
